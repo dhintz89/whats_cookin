e1a873b4346b949cdf0c3680f696ea42ca46eac0
dhintz <dhintz89@gmail.com>
2019-11-21 20:47:06 -0500

    add js to pull login params from html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "http://localhost:8000"
 const USERS_URL = `${BASE_URL}/users`
 const RECIPES_URL = `${BASE_URL}/recipes`
 const INGREDIENTS_URL = `${BASE_URL}/ingredients`
 const INSTRUCTIONS_URL = `${BASE_URL}/instructions`
 
+document.getElementById('login-submit-btn').addEventListener("click", (e) => {
+    let username = document.querySelector('#login-form .username-input').value
+    let password = document.querySelector('#login-form .password-input').value
+    console.log(`${username} ${password}`);
+});
